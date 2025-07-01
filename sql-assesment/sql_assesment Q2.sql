6aef7d03f4a03c6cd6c7d54edf923fbffb59ac7e
AlwatNisha <nishaalwat@gmail.com>
2025-07-01 21:25:04 +0530

    Add files via upload

---
+++ sql-assesment/sql_assesment Q2.sql
@@ -0,0 +1,7 @@
+create database school;
+use school;
+select * from student1;
+select dob,stdname from student1;
+select*from student1 where percentage>=80;
+select stdname,stream,percentage from student1 where percentage>80;
+select*from student1 where stream="science" and percentage>75;
